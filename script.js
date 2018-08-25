2828e5724d6896202a578a6f32a92c3067410dbe
Kir Neboula <newbie13xd@outlook.com>
2018-08-25 14:28:29 +0200

    Now somewhat works

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 "use strict";
 const titleDisplay = document.getElementById("title");
 const mainDisplay = document.getElementById("mainDisplay");
+const secondaryDisplays = document.getElementsByClassName("codeItemText");
+const nSecondaryDisplays = secondaryDisplays.length;
 
 document.addEventListener("keypress", function(event) {
-    console.log(event.keyCode);
-    titleDisplay.innerHTML = event.keyCode;
-    mainDisplay.innerHTML = event.keyCode;
-})
+    console.log(event.key);
+    titleDisplay.innerHTML = event.key;
+    mainDisplay.innerHTML = event.key;
+    for (let i = 0; i < nSecondaryDisplays; i++) {
+        switch (i) {
+            case 0:
+                secondaryDisplays[i].innerHTML = event.key;
+                break;
+        }
+    }
+});
